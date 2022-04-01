7b2ad151bbe46477f368ce3016c157a158970120
didof <didonato.fr@gmail.com>
2022-04-01 17:03:14 +0200

    remote build folder

--- build/schemas/GithubCode.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typebox_1 = require("@sinclair/typebox");
-const GithubCode = typebox_1.Type.Object({
-    code: typebox_1.Type.String(),
-});
