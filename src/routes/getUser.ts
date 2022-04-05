b9d36d5c5b167a47d8f3f920bc7a82c80b4c2215
giandata <happydaysbcn2017@gmail.com>
2022-04-05 09:58:53 +0200

    fix getUser route

--- src/routes/getUser.ts
+++ src/routes/getUser.ts
@@ -1,18 +1,27 @@
 import to from "await-to-js";
 import { AxiosResponse } from "axios";
 import { FastifyInstance } from "fastify";
+import { VerifyPayloadType } from "fastify-jwt";
 import { axiosApiGithub } from "../helpers";
+
+interface AccessTokenJWT {
+  access_token: string;
+  expires_in: number;
+  iat: number;
+}
 
 export default function getUser(server: FastifyInstance, opts: any, done: any) {
   server.get("/user", async (request, reply) => {
     let err: Error | null;
-    let access_token: any;
+    let verifyPayloadType: AccessTokenJWT | undefined;
     let response: AxiosResponse<any> | undefined;
 
-    [err, access_token] = await to(request.jwtVerify());
+    [err, verifyPayloadType] = await to<AccessTokenJWT>(request.jwtVerify());
     if (err) {
       return reply.status(401).send({ err: "Not Authenticated" });
     }
+
+    const { access_token } = verifyPayloadType!;
 
     [err, response] = await to(
       axiosApiGithub.get("/user", {
